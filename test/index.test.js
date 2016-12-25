1c70c8e06889832bf43f5979c1641d6f7ab5b161
Niklas Kiefer <niklas.kiefer@student.hpi.de>
2016-12-25 11:38:41 +0100

    add basic python execution and return it via promises

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,25 @@
 const expect = require('chai').expect;
 const feathers = require('feathers');
+const logger = require('winston');
 
 const FythonService  = require('../lib');
 
 describe('fython service', () => {
   let app, service;
 
-  beforeEach(done => {
+  beforeEach(() => {
     app = feathers();
-
-    app.use('/pythonScript', new FythonService({scriptPath: './helloWorld.py'}));
-
+    app.use('/pythonScript', new FythonService({scriptPath: 'test/helloWorld.py'}));
     service = app.service('pythonScript');
-    done();
   });
 
   it('should execute the python script properly', () => {
     expect(service).to.not.be.nil;
+    return service.create({
+      param1: 'Test'
+    }).then((result) => {
+      expect(result).to.contains('Hello World!');
+    });
   });
 
 });
