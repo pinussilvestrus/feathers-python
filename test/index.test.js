365e0263845dea34ac9c95a345ac76c8f916d924
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-03-22 21:32:17 +0100

    feat(lib): update script content on PATCH
    Relates to #2

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,58 @@
 const expect = require('chai').expect;
 const feathers = require('@feathersjs/feathers');
+const fs = require('fs');
 const logger = require('winston');
 
 const FythonService  = require('../lib');
 
-describe('fython service', () => {
+const testScript = `
+import sys as sys
+if len(sys.argv) > 1:
+  print("Hello " + sys.argv[1] + "!")
+else:
+  print("Hello World!")
+  `;
+
+const testScriptPath = 'test/helloWorld.py';
+
+describe('fython service', (done) => {
   let app, service;
 
-  beforeEach(() => {
+  beforeEach((done) => {
     //  given
     app = feathers();
-    app.use('/pythonScript', new FythonService({scriptPath: 'test/helloWorld.py'}));
-    service = app.service('pythonScript');
 
-    // assure
-    expect(service).to.not.be.undefined;
+    fs.writeFile(testScriptPath, testScript, (err) => {
+
+      if(err) {
+        expect.fail("error appeared in setup: " + err);
+      }
+
+      app.use('/pythonScript', new FythonService({scriptPath: testScriptPath}));
+      service = app.service('pythonScript');
+  
+      // assure
+      expect(service).to.not.be.undefined;
+
+      done();
+    });
+  });
+
+  afterEach((done) => {
+    // cleanup
+    fs.unlink(testScriptPath, (err) => {
+      if (err) {
+        expect.fail("error appeared in teardown: " + err);
+      }
+
+      done();
+    });
   });
 
   describe('POST', function() {
 
     it('should execute the python script properly', () => {
+      // when
       return service.create({}).then((result) => {
         // then
         expect(result).to.contain('Hello World!');
@@ -28,6 +61,7 @@ describe('fython service', () => {
 
   
     it('should execute the python script with correct first param', () => {
+      // when
       return service.create({
         param1: 'Test'
       }).then((result) => {
@@ -40,10 +74,32 @@ describe('fython service', () => {
 
 
   describe('GET', function() {
+    // when
     it('return script content', () => {
       return service.find({}).then((result) => {
         // then
         expect(result).to.contains('import sys as sys');
+      });
+    });
+  });
+  
+
+  describe('PATCH', function() {
+    // given
+    const content = '<foo>';
+
+    // when
+    it('update script content', () => {
+      return service.patch(null, {
+        content
+      }).then((result) => {
+        // then
+        expect(result).to.eql(content);
+
+        return service.find({}).then((result) => {
+          // then
+          expect(result).to.eql(content);
+        });
       });
     });
   });
