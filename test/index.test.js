7e99e4757ddee8792ac680955b82e4bcd2eb5116
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-11-23 10:37:28 +0100

    feat: integrate eslint

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,8 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const feathers = require('@feathersjs/feathers');
 const fs = require('fs');
-const logger = require('winston');
 
-const FythonService  = require('../lib');
+const FythonService = require('../lib');
 
 const testScript = `
 import sys as sys
@@ -15,27 +14,27 @@ else:
 
 const testScriptPath = 'test/helloWorld.py';
 
-describe('fython service', (done) => {
-  let app, service;
+describe('fython service', () => {
+  let app; let
+    service;
 
   beforeEach((done) => {
     //  given
     app = feathers();
 
     fs.writeFile(testScriptPath, testScript, (err) => {
-
-      if(err) {
-        expect.fail("error appeared in setup: " + err);
+      if (err) {
+        expect.fail(`error appeared in setup: ${err}`);
       }
 
       const options = {
         scriptPath: testScriptPath,
-        pythonVersion: 'v2'
+        pythonVersion: 'v2',
       };
 
       app.use('/pythonScript', new FythonService(options));
       service = app.service('pythonScript');
-  
+
       // assure
       expect(service).to.not.be.undefined;
 
@@ -47,66 +46,54 @@ describe('fython service', (done) => {
     // cleanup
     fs.unlink(testScriptPath, (err) => {
       if (err) {
-        expect.fail("error appeared in teardown: " + err);
+        expect.fail(`error appeared in teardown: ${err}`);
       }
 
       done();
     });
   });
 
-  describe('POST', function() {
-
-    it('should execute the python script properly', () => {
+  describe('POST', () => {
+    it('should execute the python script properly', () =>
       // when
-      return service.create({}).then((result) => {
+      service.create({}).then((result) => {
         // then
         expect(result).to.contain('Hello World!');
-      });
-    });
+      }));
 
-  
-    it('should execute the python script with correct first param', () => {
+    it('should execute the python script with correct first param', () =>
       // when
-      return service.create({
-        param1: 'Test'
+      service.create({
+        param1: 'Test',
       }).then((result) => {
         // then
         expect(result).to.contain('Hello Test!');
-      });
-    });
-
+      }));
   });
 
-
-  describe('GET', function() {
+  describe('GET', () => {
     // when
-    it('return script content', () => {
-      return service.find({}).then((result) => {
-        // then
-        expect(result).to.contains('import sys as sys');
-      });
-    });
+    it('return script content', () => service.find({}).then((result) => {
+      // then
+      expect(result).to.contains('import sys as sys');
+    }));
   });
-  
 
-  describe('PATCH', function() {
+  describe('PATCH', () => {
     // given
     const content = '<foo>';
 
     // when
-    it('update script content', () => {
-      return service.patch(null, {
-        content
-      }).then((result) => {
+    it('update script content', () => service.patch(null, {
+      content,
+    }).then((result) => {
+      // then
+      expect(result).to.eql(content);
+
+      return service.find({}).then((data) => {
         // then
-        expect(result).to.eql(content);
-
-        return service.find({}).then((result) => {
-          // then
-          expect(result).to.eql(content);
-        });
+        expect(data).to.eql(content);
       });
-    });
+    }));
   });
-
 });
